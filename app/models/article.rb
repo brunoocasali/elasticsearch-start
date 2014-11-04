f74a4dac0a1f362dc812141a7ace320a5f959ffe
Bruno Casali <brunoocasali@gmail.com>
2014-11-04 18:31:08 -0200

    Fix errors uninitialized constant Elasticsearch::Model::Calbacks, and moved the search.html.erb to search directory.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ require 'elasticsearch/model'
 
 class Article < ActiveRecord::Base
 	include Elasticsearch::Model
-	include Elasticsearch::Model::Calbacks
+	include Elasticsearch::Model::Callbacks
 end
 
 Article.import
