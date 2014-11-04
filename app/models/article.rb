6104e8857d79ea50392362a2f87cb53609a5d57e
Bruno Casali <brunoocasali@gmail.com>
2014-11-04 18:45:03 -0200

    Showing the highlights for the word searched. And improved the quality of search.

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,44 @@ require 'elasticsearch/model'
 class Article < ActiveRecord::Base
 	include Elasticsearch::Model
 	include Elasticsearch::Model::Callbacks
+	
+	def self.search(query)
+		__elasticsearch__.search(
+		  {
+		    query: {
+		      multi_match: {
+		        query: query,
+		        fields: ['title^10', 'text']
+		      }
+		    },
+		    highlight: {
+		      pre_tags: ['<em style="color:blue;">'],
+		      post_tags: ['</em>'],
+		      fields: {
+		        title: {},
+		        text: {}
+		      }
+		    }
+		  }
+		)
+	end
+	
+	settings index: { number_of_shards: 1 } do
+	  mappings dynamic: 'false' do
+	    indexes :title, analyzer: 'portuguese', index_options: 'offsets'
+	    indexes :text, analyzer: 'portuguese'
+	  end
+	end
+
 end
 
+# Delete the previous articles index in Elasticsearch
+Article.__elasticsearch__.client.indices.delete index: Article.index_name rescue nil
+ 
+# Create the new index with the new mapping
+Article.__elasticsearch__.client.indices.create \
+  index: Article.index_name,
+  body: { settings: Article.settings.to_hash, mappings: Article.mappings.to_hash }
+ 
+# Index all article records from the DB to Elasticsearch
 Article.import
